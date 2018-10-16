dd2a05ca528228dea9efccefc402fba8010c8058
Student <student@imac-04.local>
2018-10-16 10:56:25 +0200

    lab 2, project structure reorganized

--- WeatherApp/WeatherGetter.swift
+++ WeatherApp/WeatherGetter.swift
@@ -35,37 +35,37 @@ class WeatherGetter {
                     // Case 2: Success
                     // We got a response from the server!
                 do {
-                    // Try to convert that data into a Swift dictionary
-                    let weather = try NSJSONSerialization.JSONObjectWithData(
-                        data!,
-                        options: .MutableContainers) as! [String: AnyObject]
-                    
-                    // If we made it to this point, we've successfully converted the
-                    // JSON-formatted weather data into a Swift dictionary.
-                    // Let's print its contents to the debug console.
-                    print("Date and time: \(weather["dt"]!)")
-                    print("City: \(weather["name"]!)")
-                    
-                    print("Longitude: \(weather["coord"]!["lon"]!!)")
-                    print("Latitude: \(weather["coord"]!["lat"]!!)")
-                    
-                    print("Weather ID: \(weather["weather"]![0]!["id"]!!)")
-                    print("Weather main: \(weather["weather"]![0]!["main"]!!)")
-                    print("Weather description: \(weather["weather"]![0]!["description"]!!)")
-                    print("Weather icon ID: \(weather["weather"]![0]!["icon"]!!)")
-                    
-                    print("Temperature: \(weather["main"]!["temp"]!!)")
-                    print("Humidity: \(weather["main"]!["humidity"]!!)")
-                    print("Pressure: \(weather["main"]!["pressure"]!!)")
-                    
-                    print("Cloud cover: \(weather["clouds"]!["all"]!!)")
-                    
-                    print("Wind direction: \(weather["wind"]!["deg"]!!) degrees")
-                    print("Wind speed: \(weather["wind"]!["speed"]!!)")
-                    
-                    print("Country: \(weather["sys"]!["country"]!!)")
-                    print("Sunrise: \(weather["sys"]!["sunrise"]!!)")
-                    print("Sunset: \(weather["sys"]!["sunset"]!!)")
+//                    // Try to convert that data into a Swift dictionary
+//                    let weather = try NSJSONSerialization.JSONObjectWithData(
+//                        data!,
+//                        options: .MutableContainers) as! [String: AnyObject]
+//
+//                    // If we made it to this point, we've successfully converted the
+//                    // JSON-formatted weather data into a Swift dictionary.
+//                    // Let's print its contents to the debug console.
+//                    print("Date and time: \(weather["dt"]!)")
+//                    print("City: \(weather["name"]!)")
+//
+//                    print("Longitude: \(weather["coord"]!["lon"]!!)")
+//                    print("Latitude: \(weather["coord"]!["lat"]!!)")
+//
+//                    print("Weather ID: \(weather["weather"]![0]!["id"]!!)")
+//                    print("Weather main: \(weather["weather"]![0]!["main"]!!)")
+//                    print("Weather description: \(weather["weather"]![0]!["description"]!!)")
+//                    print("Weather icon ID: \(weather["weather"]![0]!["icon"]!!)")
+//
+//                    print("Temperature: \(weather["main"]!["temp"]!!)")
+//                    print("Humidity: \(weather["main"]!["humidity"]!!)")
+//                    print("Pressure: \(weather["main"]!["pressure"]!!)")
+//
+//                    print("Cloud cover: \(weather["clouds"]!["all"]!!)")
+//
+//                    print("Wind direction: \(weather["wind"]!["deg"]!!) degrees")
+//                    print("Wind speed: \(weather["wind"]!["speed"]!!)")
+//
+//                    print("Country: \(weather["sys"]!["country"]!!)")
+//                    print("Sunrise: \(weather["sys"]!["sunrise"]!!)")
+//                    print("Sunset: \(weather["sys"]!["sunset"]!!)")
                 }
                 catch let jsonError as NSError {
                     // An error occurred while trying to convert the data into a Swift dictionary.
